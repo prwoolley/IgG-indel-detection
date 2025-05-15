1db2771927c2e8edb15f8457ac04940b2f00d684
prwoolley <58863297+prwoolley@users.noreply.github.com>
2025-05-15 13:23:33 -0500

    Update insertion_finder_indel_v1.py

--- scripts/insertion_finder_indel_v1.py
+++ scripts/insertion_finder_indel_v1.py
@@ -35,7 +35,10 @@ def find_gap(seq):
 
 
 def main(args):
-    df_igb = pd.read_csv(base_filename + '_igblast.tsv', sep='\t')
+    igblast_tsv = args.igblast_tsv
+    
+    df_igb = pd.read_csv(igblast_tsv, sep='\t')
+    # df_igb = pd.read_csv(base_filename + '_igblast.tsv', sep='\t')
     
     df_igb = df_igb[['sequence_id', 'sequence', 'sequence_aa', 'v_call', 'v_germline_alignment',
            'v_germline_alignment_aa','fwr1', 'fwr1_aa', 'cdr1', 'cdr1_aa', 
@@ -43,8 +46,9 @@ def main(args):
    
     df_igb['VH'] = df_igb['v_call'].apply(simple_process)
     df_igb[['gap_length', 'gap_start', 'gap_end']] = df_igb['v_germline_alignment_aa'].apply(find_gap)
-    
-    df_igb.to_csv(base_filename+'_aligned.csv', index=False)
+
+    df_igb.to_csv(igblast_tsv[:-3]+'_aligned.csv', index=False)
+    # df_igb.to_csv(base_filename+'_aligned.csv', index=False)
 
 
 if __name__ == '__main__':
@@ -53,8 +57,9 @@ if __name__ == '__main__':
         epilog="""Example usage: python3 insertion_finder.py cluster_stats.csv matches.m8 vh.assembled_annotation"""
     )
     # Define a positional argument for the input file
-    parser.add_argument('cluster_stats', help='Path to the cluster_stats.csv file')
-    parser.add_argument('matches', help='Path to the matches.m8 file')
-    parser.add_argument('vh_assembled_annotation', help='Path to the assembled annotation file')
+    parser.add_argument('--igblast_tsv', help='Path to the IGBlast TSV')
+    parser.add_argument('--cluster_stats', help='Path to the cluster_stats.csv file')
+    parser.add_argument('--matches', help='Path to the matches.m8 file')
+    parser.add_argument('--vh_assembled_annotation', help='Path to the assembled annotation file')
     args = parser.parse_args()
     main(args)
